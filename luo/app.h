13c37fd2f2860140eae67990716afb7f7046a011
luozuhua <lzhbuct@163.com>
2020-11-30 22:55:04 +0800

    modefined add.h

---
+++ luo/app.h
@@ -0,0 +1,17 @@
+355H9V1T MINGW64 /d/Software1/Githubproject/luozuhua1
+$ git branch
+fatal: not a git repository (or any of the parent directories): .git
+
+lzh@LAPTOP-355H9V1T MINGW64 /d/Software1/Githubproject/luozuhua1
+$ git branch aaa
+fatal: not a git repository (or any of the parent directories): .git
+
+lzh@LAPTOP-355H9V1T MINGW64 /d/Software1/Githubproject/luozuhua1
+$ git checkout aa
+fatal: not a git repository (or any of the parent directories): .git
+
+lzh@LAPTOP-355H9V1T MINGW64 /d/Software1/Githubproject/luozuhua1
+$ git checkout -b aa
+fatal: not a git repository (or any of the parent directories): .git
+
+lzh@LAPTOP-355H9V1T MINGW64 /d/Software1/Gi
